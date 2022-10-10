a58f751f94b61dab7dd68e79d980fc67bb7ca7cd
melaniewalsh <melanie.walsh@cornell.edu>
2022-10-10 13:23:38 -0700

    add exercises

---
+++ Exercises/Week3-Part1-Exercises/NP-Data.R
@@ -0,0 +1,25 @@
+# Load data
+np_data <- read.csv("https://raw.githubusercontent.com/melaniewalsh/Neat-Datasets/main/1979-2020-National-Park-Visits-By-State.csv ")
+
+# View this dataframe by clicking on the spreadsheet icon in the Global Environment
+
+# Install tidyverse
+install.packages("tidyverse")
+
+# Load dplyr library
+library(dplyr)
+
+# Select 2 columns from the data and save to the variable smaller_df
+Your code here
+
+# View this dataframe by clicking on the spreadsheet icon in the Global Environment
+
+# Filter the dataframe for only values in the state of Washington and save to the variable
+# Hint: Think about the special equals sign that you will need to use!
+Your code here
+
+# View this dataframe by clicking on the spreadsheet icon in the Global Environment
+
+# Calculate the sum total of RecreationVisits to Washington by using summarize() on the smaller dataframe wa_parks
+Your code here
+                             
---
+++ Exercises/Week3-Part1-Exercises/Your-Own-Cowsay.R
@@ -0,0 +1,17 @@
+# Make Your Own Version of the Cowsay Function called "shout"
+# Use str_glue to print the message
+# Part of the function is written for you
+
+# Install and load stringr, styler
+install.packages("stringr")
+install.packages("styler")
+library("stringr")
+
+
+shout <- function(#your code here) #your code here
+  
+  str_glue("'{message}' \n\n\n -💩")
+
+# your code here
+
+shout("Hi class")
