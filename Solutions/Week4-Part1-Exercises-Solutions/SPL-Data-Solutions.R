df55bc632489f68c18e0dcfb2d1716d9293deb74
melaniewalsh <melanie.walsh@cornell.edu>
2022-10-17 13:10:58 -0700

    add Week 4 exercises and solutions

---
+++ Solutions/Week4-Part1-Exercises-Solutions/SPL-Data-Solutions.R
@@ -0,0 +1,46 @@
+# SPL Data — Queering the Catalog
+
+#Load the stringr library
+# install.packages("stringr")
+library("stringr")
+
+# Load the Seattle Public Library Data
+# https://github.com/melaniewalsh/Neat-Datasets/raw/main/Checkouts_by_Title_2018-2022_15-checkouts.csv
+spl_data <- read.csv("https://github.com/melaniewalsh/Neat-Datasets/raw/main/Checkouts_by_Title_2018-2022_15-checkouts.csv", stringsAsFactors = FALSE)
+
+# Load the dplyr library
+# Load the tidyverse library
+library(dplyr)
+library(tidyverse)
+
+# Exercise 1: Filter for all of the Lord of the Rings books and save to a variable filtered titles 
+filtered_titles <- spl_data %>% filter(str_detect(Title, "Lord of the Rings"))
+
+# Now search for a title that you are interested in!
+
+##### Subject Headings
+
+# How has the Seattle Public Library handled changes in LOC Subject Headings?
+
+# Exercise 2: Filter for all books that include a certain term (e.g. "aliens") in the Subject Heading
+term1_subjects <- spl_data %>% filter(str_detect(Subjects, "(?i)aliens"))
+
+# Exercise 3: Filter for all books that include (e.g. "noncitizens") in the Subject Heading
+term2_subjects <- spl_data %>% filter(str_detect(Subjects, "(?i)noncitizens"))
+
+# Let's compare the books that have term 1-related Subject Headings and the books that have term 2-related Subject Headings
+
+# Exercise 4: Group the term1_subejcts and term2_subjects by subject heading OR by year, and then use the function n() to count how frequent each heading is OR how many headings appeared each year
+
+term1_subjects_df <- term1_subjects %>%
+  group_by(CheckoutYear) %>%
+  summarize(num_records = n())
+
+term2_subjects_df <- term2_subjects %>%
+  group_by(CheckoutYear) %>%
+  summarize(num_records = n())
+
+# What additional work might we need to do to analyze Subject Headings?
+
+# Bonus: You can use a tidyverse function separate_rows() to split a column with multiple items into individual rows with each item. Check it out! 
+spl_data_subjects <- spl_data %>% separate_rows(Subjects, sep = ",")
